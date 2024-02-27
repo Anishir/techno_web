63ba19875500b75a02a12703817aa24449aa4fb9
Anishir <92084321+Anishir@users.noreply.github.com>
2024-02-27 12:08:42 +0100

    Update functions.py

--- services/functions.py
+++ services/functions.py
@@ -3,15 +3,15 @@ from app.database import database
 
 
 
-def save_task(add_new_book: Book) -> Book:
+def save_book(add_new_book: Book) -> Book:
     database["books"].append(add_new_book)
     return add_new_book
 
 
-def afficher_liste_et_nombre_livres() -> list[Book],int:  
+def display_list_of_book_and_number() -> list[Book],int:  
     for livre in database["livres"]:
        books_liste= [livre]
         
     total_livres = len(database["livres"])
-    print(f"Nombre total de livres : {total_livres}")
+    print(f"the number of books is : {total_livres}")
     return books_liste, total_livres
